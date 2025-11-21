168fa762f1935945fcc35052afd466fe0b438c50
Emre ERTUĞRUL <emreer17@outlook.com>
2025-11-22 02:48:57 +0300

    experiencelogo style update

--- src/components/LogoIcon.jsx
+++ src/components/LogoIcon.jsx
@@ -1,19 +1,9 @@
+import "../styles/LogoIcon.css";
 export default function LogoIcon({ src, alt }) {
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          overflow: "hidden",
-          width: "50px",
-          height: "50px",
-          marginTop: "8px",
-          marginLeft: "5px",
-        }}
-      >
-        <img style={{ width: "100%", height: "100%" }} src={src} alt={alt} />
+      <div className="logo-icon-container">
+        <img src={src} alt={alt} />
       </div>
     </>
   );
